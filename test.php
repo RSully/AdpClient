f407c8036bd22eac1648b0a440b29c8e3e82b12a
Ryan <rsul.dev@me.com>
2013-06-07 14:08:49 -0400

    Basic CLI parser and auth file

--- test.php
+++ test.php
@@ -1,21 +1,55 @@
 <?php
 require_once __DIR__ . '/AdpClient.class.php';
 date_default_timezone_set('America/New_York');
-$tmp = __DIR__ . '/tmp-data';
 
+function handleInit()
+{
+	global $adp;
 
-$adp = new AdpClient('USERNAME', 'PASSWORD');
-$data = $adp->getActivityJournal();
-// $data = $adp->sendClock('OUT');
+	$authData = explode("\n", file_get_contents(__DIR__ . '/auth'));
+	$auth = explode('::', array_shift($authData));
 
+	$opts = array();
 
-// Formatting test
+	while (($line = array_shift($authData))) {
+		$opt = explode('::', $line);
+		if ($opt[0] == 'proxy') {
+			$opts[CURLOPT_PROXYTYPE] = CURLPROXY_SOCKS5;
+			$opts[CURLOPT_PROXY] = $opt[1];
+		} else {
+			$opts[$opt[0]] = $opt[1];
+		}
+	}
 
-if (isset($data)) {
-	file_put_contents($tmp, $data);
-	file_put_contents($tmp . '-' . time(), $data);
-} else {
-	$data = file_get_contents($tmp);
+	$adp = new AdpClient($auth[0], $auth[1], $opts);
 }
 
+function handleCommand()
+{
+	global $adp, $argv, $argc;
+
+	if (count($argv) < 2) {
+		printf("Usage: %s [command] [params]\n", $argv[0]);
+		exit;
+	}
+
+	if ($argv[1] == 'clock') {
+		$res = $adp->sendClock(strtoupper($argv[2]));
+	} else if ($argv[1] == 'journal') {
+		// shows by default
+		$res = 'Timesheet below';
+	}
+
+	return $res;
+}
+
+handleInit();
+$data = handleCommand();
 var_dump($data);
+
+$data = $adp->getActivityJournal();
+print_r($data);
+
+
+// Example call
+// $data = $adp->sendClock('OUT');
