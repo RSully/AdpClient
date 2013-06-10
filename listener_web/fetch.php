ab3aa70f11f3dba2a82d4625acc27c170affa7cc
Ryan <rsul.dev@me.com>
2013-06-10 12:02:38 -0400

    Going to use SQL instead of queue file

--- listener_web/fetch.php
+++ listener_web/fetch.php
@@ -1,14 +1,3 @@
 <?php
 // File to GET from cron
-require_once __DIR__ . '/../include/ListenerWebQueue.class.php';
 
-$queue = ListenerWebQueue::shared();
-
-if (($queue->lock())) {
-	echo $queue->read();
-	$queue->empty();
-
-	$queue->unlock();
-} else {
-	echo json_encode(false);
-}
--- listener_web/index.php
+++ listener_web/index.php
@@ -1,2 +1,12 @@
 <?php
 // File that adds to the queue
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ADP Clock</title>
+</head>
+<body>
+	<h1>ADP Clock</h1>
+</body>
+</html>
