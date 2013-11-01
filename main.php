c8bb296617313244edfa8d2de249d221cb738763
Ryan <rsul.dev@me.com>
2013-11-01 08:28:09 -0400

    Fix formatting and add option for interface

--- main.php
+++ main.php
@@ -21,6 +21,7 @@ $args->addOption(array('username', 'u'), 'Username');
 $args->addOption(array('password', 'p'), 'Password');
 
 $args->addOption(array('proxy'), 'SOCKS5 proxy');
+$args->addOption(array('interface'), 'Networking interface');
 
 $args->addFlag(array('adp-timesheet'), 'Show ADP timesheet');
 $args->addFlag(array('adp-journal'), 'Show ADP journal');
@@ -40,10 +41,13 @@ $args->parse();
  * Load extra options from authfile
  */
 
-if (isset($args['configfile'])) {
+if (isset($args['configfile']))
+{
 	$authfile = json_decode(file_get_contents($args['configfile']), true);
-	foreach ($authfile as $k => $v) {
-		if (!isset($args[$k])) {
+	foreach ($authfile as $k => $v)
+	{
+		if (!isset($args[$k]))
+		{
 			$args[$k] = $v;
 		}
 	}
@@ -58,11 +62,13 @@ $required = array(
 	'adp-timesheet|adp-journal|adp-clock-in|adp-clock-out'
 );
 
-if ($args['help'] || count($args->getArguments()) < 1 || ($missing = missingArgs($args, $required))) {
+if ($args['help'] || count($args->getArguments()) < 1 || ($missing = missingArgs($args, $required)))
+{
 	printf("Quick docs:\n\n");
 	echo $args->getHelpScreen() . "\n\n";
 
-	if (isset($missing) && count($missing) > 0) {
+	if (isset($missing) && count($missing) > 0)
+	{
 		echo "Missing:\n\n";
 		echo implode("\n", $missing) . "\n\n";
 	}
@@ -74,14 +80,22 @@ $user = null;
 $pass = null;
 $options = array();
 
-if ($args['username'] && $args['password']) {
+if ($args['username'] && $args['password'])
+{
 	$user = $args['username'];
 	$pass = $args['password'];
 }
 
-if ($args['proxy']) {
+
+if ($args['proxy'])
+{
 	$options[CURLOPT_PROXYTYPE] = CURLPROXY_SOCKS5;
 	$options[CURLOPT_PROXY] = $args['proxy'];
+}
+
+if ($args['interface'])
+{
+	$options[CURLOPT_INTERFACE] = $args['interface'];
 }
 
 /**
@@ -90,26 +104,33 @@ if ($args['proxy']) {
 
 $adp = new AdpClient($user, $pass, $options);
 
-if ($args['adp-timesheet']) {
+if ($args['adp-timesheet'])
+{
 	$timesheet_date_begin = new DateTime('Monday this week');
 	$timesheet_date_end = null;
 
-	if ($args['at-date-begin']) {
+	if ($args['at-date-begin'])
+	{
 		$timesheet_date_begin = new DateTime($args['at-date-begin']);
-		if ($args['at-date-end']) {
+		if ($args['at-date-end'])
+		{
 			$timesheet_date_end = new DateTime($args['at-date-end']);
 		}
 	}
 	$adp->showTimesheet($timesheet_date_begin, $timesheet_date_end);
 }
 
-if ($args['adp-clock-in']) {
+if ($args['adp-clock-in'])
+{
 	$adp->sendClock("IN");
-} else if ($args['adp-clock-out']) {
+}
+else if ($args['adp-clock-out'])
+{
 	$adp->sendClock("OUT");
 }
 
-if ($args['adp-journal']) {
+if ($args['adp-journal'])
+{
 	$adp->showActivityJournal();
 }
 
@@ -124,27 +145,33 @@ function missingArgs($args, $required)
 {
 	$missing = array();
 
-	foreach ($required as $reqStr) {
+	foreach ($required as $reqStr)
+	{
 		$req = explode('|', $reqStr);
 		$reqGood = false;
 
-		foreach ($req as $optionsStr) {
+		foreach ($req as $optionsStr)
+		{
 			$options = explode(',', $optionsStr);
 			$optionsNeed = count($options);
 			$optionsHave = 0;
 
-			foreach ($options as $option) {
-				if ($args[$option]) {
+			foreach ($options as $option)
+			{
+				if ($args[$option])
+				{
 					$optionsHave++;
 				}
 			}
 
-			if ($optionsNeed == $optionsHave) {
+			if ($optionsNeed == $optionsHave)
+			{
 				$reqGood = true;
 			}
 		}
 
-		if (!$reqGood) {
+		if (!$reqGood)
+		{
 			$missing[] = $reqStr;
 		}
 	}
